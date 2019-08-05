79072f7d05cef492975eca0632ff4c9c45a8b2c3
Alexander Militar <militara106@gmail.com>
2019-08-04 20:00:54 -0700

    Javascript tested, firebase and input validation working, BUG: next arrival time not working when calculated past 23:59

--- assets/app.js
+++ assets/app.js
@@ -22,99 +22,119 @@ $(document).ready(function () {
     var tNext;
     var tAway;
 
+    // Validate input
+    function validate() {
+        tTime = $('#input-time').val().trim();
+        tFreq = $('#input-frequency').val().trim();
+
+        if (moment(tTime, "HH:mm").isValid() == false || tFreq > 60 || isNaN(tFreq) == true) {
+            console.log("Validated false");
+            return false;
+        } else {
+            console.log("Validated true");
+            return true;
+        }
+    }
+
 
     // Submit
     $("#submit").on("click", function () {
         // Prevent Default Submit button action
         event.preventDefault();
 
-        // Variabels from form
-        tName = $('#input-name').val().trim();
-        tDest = $('#input-destination').val().trim();
+        // Check for valid input
+        if (validate() == true) {
+            // Variabels from form
+            tName = $('#input-name').val().trim();
+            tDest = $('#input-destination').val().trim();
 
-        tTime = $('#input-time').val().trim();
-        tTime = moment(tTime).format("HH:mm");
+            tTime = $('#input-time').val().trim();
+            tTime = moment(tTime, "HH:mm").format("HH:mm");
 
-        tFreq = $('#input-frequency').val().trim();
-        tFreq = moment(tFreq).format("HH:mm");
+            tFreq = $('#input-frequency').val().trim();
 
-        console.log(tName);
-        console.log(tDest);
-        console.log(tTime);
-        console.log(tFreq);
+            console.log("Name Input: " + tName);
+            console.log("Destination Input: " + tDest);
+            console.log("First Train: " + tTime);
+            console.log("Frequency Input: " + tFreq);
 
-        // Calculate next arrival
-        tNext = tTime;
-        while (tNext != moment().format("HH:mm")) {
-            if (tNext < moment().format("HH:mm")) {
-                tNext = tTime + tFreq;
-            } else {
-                tNext = tTime;
+            // Calculate next arrival
+            tNext = tTime;
+            while (moment(tNext, "HH:mm") < moment()) {
+                tNext = moment(tNext, "HH:mm").add(tFreq, "minutes");
+                console.log("Calculating next arrival: " + moment(tNext).format("HH:mm"));
             }
+            // Calculate minutes away
+            tAway = moment(tNext, "HH:mm").diff(moment(), "minutes");
+            console.log("Minutes Away: " + tAway);
+
+            // Push to database
+            database.ref().push({
+                name: tName,
+                destination: tDest,
+                frequency: tFreq,
+                time: tTime
+            });
+
+            // Clear Form
+            $('#input-name').val('');
+            $('#input-destination').val('');
+            $('#input-time').val('');
+            $('#input-frequency').val('');
+        } else {
+            alert("Time or Frequency not in correct format");
         }
-
-        // Calculate minutes away
-        tAway = moment().diff(moment(tNext, "mm"), "minutes");
-        console.log("Minutes Away: " + tNext);
-
-        // Push to database
-        database.ref().push({
-            name: tName,
-            destination: tDest,
-            frequency: tFreq,
-            next: tNext,
-            away: tAway,
-        });
-
-        // Clear Form
-        $('#input-name').val('');
-        $('#input-destination').val('');
-        $('#input-time').val('');
-        $('#input-frequency').val('');
     });
 
     database.ref().on("child_added", function (childSnapshot) {
         // Log everything that's coming out of snapshot
 
-        var array = [childSnapshot.val().name, childSnapshot.val().destination, childSnapshot.val().frequency, childSnapshot.val().next, childSnapshot.val().away]
-
-        var row = $("<tr>");
-
-        for (var i = 0; i < array.length; i++) {
-            console.log(array[i]);
-            var cell = $("<td>");
-            $(cell).text(array[i]);
-            $(row).append(cell);
-        }
-
-        $("#currentTrian").append(row);
-
-        // console.log(childSnapshot.val().name);
-        // console.log(childSnapshot.val().destination);
-        // console.log(childSnapshot.val().frequency);
-        // console.log(childSnapshot.val().next);
-        // console.log(childSnapshot.val().away);
+        // var array = [childSnapshot.val().name, childSnapshot.val().destination, childSnapshot.val().frequency, tNext, tAway];
 
         // var row = $("<tr>");
-        // var cell1 = $("<td>");
-        // var cell2 = $("<td>");
-        // var cell3 = $("<td>");
-        // var cell4 = $("<td>");
-        // var cell5 = $("<td>");
 
-        // $(cell1).text(childSnapshot.val().name);
-        // $(cell2).text(childSnapshot.val().destination);
-        // $(cell3).text(childSnapshot.val().frequency);
-        // $(cell4).text(childSnapshot.val().next);
-        // $(cell5).text(childSnapshot.val().away);
+        // for (var i = 0; i < array.length; i++) {
+        //     console.log(array[i]);
+        //     var cell = $("<td>");
+        //     $(cell).text(array[i]);
+        //     $(row).append(cell);
+        // }
 
-        // $(row).append(cell1);
-        // $(row).append(cell2);
-        // $(row).append(cell3);
-        // $(row).append(cell4);
-        // $(row).append(cell5);
+        // $("#currentTrian").append(row);
 
-        // $("#currentTrain").append(row);
+        console.log("Data Pulled");
+        tNext = childSnapshot.val().time;
+        tFreq = childSnapshot.val().frequency;
+
+        // Calculate Next Arrival
+        while (moment(tNext, "HH:mm") < moment()) {
+            tNext = moment(tNext, "HH:mm").add(tFreq, "minutes");
+            console.log("Calculating next arrival: " + moment(tNext).format("HH:mm"));
+        }
+        // Calculate minutes away
+        tAway = moment(tNext, "HH:mm").diff(moment(), "minutes");
+        console.log("Minutes Away: " + tAway);
+
+        var row = $("<tr>");
+        var cell1 = $("<td>");
+        var cell2 = $("<td>");
+        var cell3 = $("<td>");
+        var cell4 = $("<td>");
+        var cell5 = $("<td>");
+
+        $(cell1).text(childSnapshot.val().name);
+        $(cell2).text(childSnapshot.val().destination);
+        $(cell3).text(childSnapshot.val().frequency);
+        $(cell4).text(moment(tNext).format("HH:mm"));
+        $(cell5).text(tAway);
+
+        $(row).append(cell1);
+        $(row).append(cell2);
+        $(row).append(cell3);
+        $(row).append(cell4);
+        $(row).append(cell5);
+
+        $("#currentTrain").append(row);
         // Handle the errors
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
